23b4be761f8cc02d13eb75b62a4c5a41c5aa0113
PhuongNM <phuongnm.bkhn@gmail.com>
2022-04-05 16:08:38 +0900

    fix bug script

--- scripts/iwslt14deen_data_prepare.sh
+++ scripts/iwslt14deen_data_prepare.sh
@@ -1,8 +1,8 @@
 # prepare iwslt 2014 de-en original data 
 
 CUR_DIR=$(pwd) 
-cd ./scripts/ && wget https://raw.githubusercontent.com/pytorch/fairseq/master/examples/translation/prepare-iwslt14.sh && cd $CUR_DIR
-cd ./data && bash ../scripts/prepare-iwslt14.sh && cd CUR_DIR
+# cd ./scripts/ && wget https://raw.githubusercontent.com/pytorch/fairseq/master/examples/translation/prepare-iwslt14.sh && cd $CUR_DIR
+cd ./data && bash ../scripts/prepare-iwslt14.sh && cd $CUR_DIR
 
 mv data/iwslt14.tokenized.de-en data/iwslt14deen
 rm -r data/iwslt14deen/tmp
